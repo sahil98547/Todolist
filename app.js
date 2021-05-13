bea85e9c682b4b2b927c1330dc601c936e31b270
Sahil <80048076+sahil98547@users.noreply.github.com>
2021-05-13 21:19:37 +0530

    Todolist
    This is todolist and i make this with help of backend.

---
+++ app.js
@@ -0,0 +1,56 @@
+//jshint esversion:6
+
+const bodyParser = require("body-parser");
+const express = require("express");
+const date = require(__dirname + "/date.js");
+
+
+const app = express();
+
+const items = ["Study", "Lunch"];
+const workItems = [];
+app.set('view engine', 'ejs');
+
+// app.use(express.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+app.get("/", function(req, res) {
+
+    const day = date.getDate();
+
+    res.render("list", { listTitle: day, newListItems: items });
+
+});
+
+
+
+app.post("/", function(req, res) {
+    // req.body = {newItem, list}
+
+    const item = req.body.newItem;
+
+    if (req.body.list === "Work") {
+        workItems.push(item);
+        res.redirect("/work");
+    } else {
+        items.push(item);
+        res.redirect("/");
+    }
+
+});
+
+app.get("/work", function(req, res) {
+    res.render("list", { listTitle: "Work List", newListItems: workItems })
+});
+
+
+app.get("/about", function(req, res) {
+    res.render("about");
+});
+
+
+app.listen(process.env.PORT || 3000, function() {
+    console.log("Server started on port 3000.");
+
+});
